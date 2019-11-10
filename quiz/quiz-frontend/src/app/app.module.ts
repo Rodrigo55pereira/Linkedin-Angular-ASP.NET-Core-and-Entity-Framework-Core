f5af7ff45d7c6f5e7c4b45544ada8a6abc0827d5
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2019-11-10 16:13:47 -0300

    Atualização

--- quiz/quiz-frontend/src/app/app.module.ts
+++ quiz/quiz-frontend/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule, MatCardModule, MatInputModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
