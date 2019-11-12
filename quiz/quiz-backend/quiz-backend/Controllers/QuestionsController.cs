8e9b5f02b81014c1edd9bfb2e9281a3482c81000
Rodrigo55pereira <rodrigo55pereira@gmail.com>
2019-11-11 21:02:11 -0300

    criação do projeto vs

---
+++ quiz/quiz-backend/quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using quiz_backend.Models;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : Controller
+    {
+        readonly QuizContext context;
+
+        public QuestionsController(QuizContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Question> Get()
+        {
+            return new Question[] {
+                new Question() { Text = "hello"},
+                new Question() { Text = "hi"}
+            };
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Question question)
+        {
+            context.Questions.Add(question);
+            context.SaveChanges();
+        }
+    }
+}
